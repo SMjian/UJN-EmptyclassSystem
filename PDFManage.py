c86962c708cea2a72537920091aebfe3bf31cef8
SMJian <15954515557@163.com>
2019-10-02 16:06:11 +0800

    生成EXCEL表格

--- PDFManage.py
+++ PDFManage.py
@@ -10,15 +10,75 @@ import sys
 import time
 import pdfplumber
 
-"""***************************挺重要的文件目录所在地*********************************"""
 Root_dir = r'./学工在线各部干事课表'
+"""保存有空课的学生的名字"""
+table1 = [[[], [], [], [],[]],
+          [[], [], [], [], []],
+          [[], [], [], [], []],
+          [[], [], [], [], []]]
+"""创造 0 1码"""
+excel = [[0,0,0,0,0],
+         [0,0,0,0,0],
+         [0,0,0,0,0],
+         [0,0,0,0,0]]
+
+#创建excel表
+def CreatExcel():
+    To_nrows = ['星期一', '星期二', '星期三', '星期四', '星期五']
+    To_ncols = ['上午12节', '上午34节', '下午12节', '下午34节']
+    style = xlwt.XFStyle()  # 初始化样式
+    al = xlwt.Alignment()
+    al.horz = 0x02  # 设置水平居中
+    al.vert = 0x01  # 设置垂直居中
+    al.wrap = 1     # 自动换行
+    style.alignment = al
+    book = xlwt.Workbook()  # 新建工作簿
+    table = book.add_sheet('main', cell_overwrite_ok=True)  # 添加主要的表格信息
+    book.save(filename_or_stream='output.xls')
+    data = xlrd.open_workbook('output.xls', formatting_info=True)
+    excel = copy(wb=data)  # 完成xlrd对象向xlwt对象转换
+    excel_table = excel.get_sheet(0)  # 获得要操作的页
+    table = data.sheets()[0]
+    nrows = table.nrows  # 获得行数
+    ncols = table.ncols  # 获得列数
+    x = y = 1
+    for i in To_nrows:
+        excel_table.write(0, x, i,style)
+        x += 1
+    for i in To_ncols:
+        excel_table.write(y, 0, i,style)
+        y += 1
+    excel.save('output.xls')
+
+#向Excel中写入数据
+def WriteExcel(index):
+    data = xlrd.open_workbook('output.xls', formatting_info=True)
+    excel = copy(wb=data)  # 完成xlrd对象向xlwt对象转换
+    excel_table = excel.get_sheet(0)  # 获得要操作的页
+    table = data.sheets()[0]
+    style = xlwt.XFStyle()  # 初始化样式
+    al = xlwt.Alignment()
+    al.horz = 0x02  # 设置水平居中
+    al.vert = 0x01  # 设置垂直居中
+    al.wrap = 1     # 自动换行
+    style.alignment = al
+    nrows = table.nrows  # 获得行数
+    ncols = table.ncols  # 获得列数
+    x = y = 1
+    for num_x in index:
+        for num_y in num_x:
+            excel_table.write(y,x,num_y,style)
+            excel_table.col(x).width = 8888
+            x += 1
+        x = 1
+        y += 1
+    excel.save('output.xls')
+
+
 
 """原本程序的代码"""
 importlib.reload(sys)
 timel = time.time()
-
-"""创造 0 1码"""
-excel = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
         
 """获得根文件名"""
 def fileManage(rootdir):
@@ -44,22 +104,20 @@ def Save(EXCEL,addsname,F,txtname):
 
 """主要读取PDF函数"""
 def tableToMatrix(rootdir,file,F):
-
     num_x = 0
     num_y = 0
-    
     """检测是否为异常PDF     1为异常   0为非异常"""
     tap = 0
-
     gets_pdf = file
-    
+
     """将PDF转化为01矩阵"""
     with pdfplumber.open(rootdir+'\\'+gets_pdf) as pdf:
     
         first_page = pdf.pages[0]
 
         text = first_page.extract_text()
-
+        pos = text.rfind('课表')
+        names = text[:pos]
         tables = first_page.extract_tables()
         for table in tables:
             for index_r, row in enumerate(table):
@@ -73,6 +131,7 @@ def tableToMatrix(rootdir,file,F):
                         num_x+=1
                     else:
                         excel[num_y][num_x] = 0
+                        table1[num_y][num_x].append(names+'|')
                         num_x+=1
                 num_x = 0
                 num_y+=1
@@ -118,5 +177,7 @@ if __name__=='__main__':
                 pass                                                    #错误的是python处理文件时出来的直接跳过
             else:
                 print(F+'   '+file+'    已完成'+'\n')                  #打点逐个输出查看错误
-    ErrorManage.close()      
+    ErrorManage.close()
+    CreatExcel()
+    WriteExcel(table1)
     print('全部完成')
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# UJN-EmptyclassSystem
+# UJN-EmptyclassSyste
 
+### 环境配置
+1. python3.6
+2. pdfplumber
+3. xlrd，xlwt，xlutils
+
+---
+### 用法
+将各个部门放入**学工在线各部干事课表**中
+<img src='1.png'>
